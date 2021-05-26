0afde4bf624e1a763fd5b62865dfcf26e17c6166
Shen Li <shenli@fb.com>
2021-05-26 19:04:26 -0400

    Adding RecSys numbers

--- benchmark/plot/draw_gpt.py
+++ benchmark/plot/draw_gpt.py
@@ -7,7 +7,7 @@ colors = [
     [239/256.0, 74/256.0, 40/256.0],
 ]
 
-WIDTH = 0.5
+WIDTH = 0.36
 SHOW = False
 FONT = {'fontname':'Times New Roman', 'size':22}
 
@@ -16,166 +16,121 @@ FONT = {'fontname':'Times New Roman', 'size':22}
 
 data_fwd_mean = [
     [
+        0, 
+        0, 
+        0,
+        0, 
         122653.87 / 1e3,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
     ], # CPU RPC
     [
-        122261.95 / 1e3,
+        537652.98 / 1e3,
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
     ], # CUDA RPC
     [
-        122,
-        122,
-        122,
-        69.28,
-        51.05,
-        44.73,
-        44.73,
-        58.16,
+        0,
+        53.7,
+        36.59,
+        39.35,
+        49.89,
     ], # Pipeline
 ]
 
 data_fwd_stdv = [
     [
+        0, 
+        0, 
+        0, 
+        0,
         102.89 / 1e3,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
     ], # CPU RPC
     [
-        166.92 / 1e3,
+        771.32 / 1e3,
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
     ], # CUDA RPC
     [
         0,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        1.53,
+        0.88, 
+        3.99, 
+        1.27, 
     ], # Pipeline
 ]
 
 data_comm_mean = [
     [
+        0, 
+        0, 
+        0, 
+        0,
         78920.51 / 1e3,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
     ], # CPU RPC
     [
-        5592.08 / 1e3,
+        19293.20 / 1e3,
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
     ], # CUDA RPC
     [
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
         0,
+        0, 
     ], # Pipeline
 ]
 
 data_comm_stdv = [
     [
+        0, 
+        0, 
+        0,
+        0, 
         89.53 / 1e3,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
     ], # CPU RPC
     [
-        17.95 / 1e3,
+        70.64 / 1e3,
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
     ], # CUDA RPC
     [
         0,
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
         0, 
     ], # Pipeline
 ]
 
 data_bwd_mean = [
     [
+        0, 
+        0, 
+        0,
+        0, 
         330225.75 / 1e3,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
     ], # CPU RPC
     [
-        240950.93 / 1e3,
+        797521.04 / 1e3,
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
     ], # CUDA RPC
     [
-        122,
-        122,
-        122,
-        69.28,
-        51.05,
-        44.73,
-        44.73,
-        58.16,
+        0,
+        152.22,
+        115.86,
+        106.92,
+        116.95,
     ], # Pipeline
 ]
 
@@ -184,47 +139,38 @@ data_bwd_stdv = [
         209.06 / 1e3,
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
         0, 
     ], # CPU RPC
     [
-        10871.85 / 1e3,
+        317.87 / 1e3,
         0, 
         0, 
         0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        0,
     ], # CUDA RPC
     [
         0,
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
-        0, 
+        3.24,
+        3.39, 
+        1.91, 
+        1.5, 
     ], # Pipeline
 ]
 
 
 def plot_nlp(x_name, y_lim):
     plt.figure(figsize=(5.3, 4))
-    xs = np.asarray(range(8))
+    xs = np.asarray(range(5))
 
 
     for i in range(1, 3):
-        fwd = np.asarray(data_fwd_mean[i]) 
-        com = np.asarray(data_comm_mean[i]) 
-        bwd = np.asarray(data_bwd_mean[i])
-        fwd_stdv = np.asarray(data_fwd_stdv[i]) 
-        com_stdv = np.asarray(data_comm_stdv[i]) 
-        bwd_stdv = np.asarray(data_bwd_stdv[i])
+        fwd = np.flip(np.asarray(data_fwd_mean[i])) 
+        com = np.flip(np.asarray(data_comm_mean[i]))
+        bwd = np.flip(np.asarray(data_bwd_mean[i]))
+        fwd_stdv = np.flip(np.asarray(data_fwd_stdv[i]))
+        com_stdv = np.flip(np.asarray(data_comm_stdv[i]))
+        bwd_stdv = np.flip(np.asarray(data_bwd_stdv[i]))
 
         fwd += com
 
@@ -240,13 +186,13 @@ def plot_nlp(x_name, y_lim):
 
 
     color_handles = []
-    color_handles.append(plt.bar([4], [0], color=colors[1]))
-    color_handles.append(plt.bar([4], [0], color=colors[2]))
+    color_handles.append(plt.bar([20], [0], color=colors[1]))
+    color_handles.append(plt.bar([2], [0], color=colors[2]))
     color_names = ["RPC", "Pipeline"]
 
     hatch_handles = []
-    hatch_handles.append(plt.bar([4], [0], hatch="///", color="white"))
-    hatch_handles.append(plt.bar([4], [0], hatch="\\\\\\", color="white"))
+    hatch_handles.append(plt.bar([2], [0], hatch="///", color="white"))
+    hatch_handles.append(plt.bar([2], [0], hatch="\\\\\\", color="white"))
     hatch_names = ["FWD", "BWD"]
 
     def interleave(l1, l2):
@@ -262,21 +208,21 @@ def plot_nlp(x_name, y_lim):
     )
 
 
-    plt.xticks(xs, ["1", "2", "4", "8", "16", "32", "64", "128"], **FONT)
+    plt.xticks(xs, ["1", "2", "4", "8", "128"], **FONT)
     plt.yticks(**FONT)
 
     plt.xlabel(x_name, **FONT)
     plt.ylabel("Delay (Second)", **FONT)
 
     plt.ylim(y_lim)
-    #plt.xlim([-0.5, 3.5])
+    plt.xlim([-0.5, 4.5])
 
     #plt.yscale('log')
 
     #plt.show()
     plt.savefig(f"../images/gpt.pdf", bbox_inches='tight')
 
-plot_nlp(x_name = "chunks", y_lim = [10, 700])
+plot_nlp(x_name = "Micro-Batch Size", y_lim = [0, 2000])
 
 
 
